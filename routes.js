bad13767a3af77da372cbd66b0c8a18c49f04338
Glitch (hammerhead-mercurial-collard) <none>
2024-08-26 23:47:15 +0000

    📯👴 Checkpoint ./routes.js:69129952/747

--- routes.js
+++ routes.js
@@ -7,7 +7,14 @@ import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
 router.get("/about", aboutController.index);
@@ -24,10 +31,5 @@ router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
 
-router.get("/", accountsController.index);
-router.get("/login", accountsController.login);
-router.get("/signup", accountsController.signup);
-router.get("/logout", accountsController.logout);
-router.post("/register", accountsController.register);
-router.post("/authenticate", accountsController.authenticate);
+
 
