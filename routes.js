e3b1d1da09a0189389ffc0e416053e519af03179
Glitch (hammerhead-mercurial-collard) <none>
2024-08-26 23:35:13 +0000

    🎙⛈ Checkpoint ./views/partials/welcome-menu.hbs:69129952/1069 ./routes.js:69129952/531

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
 import { reportController } from "./controllers/report-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
@@ -23,5 +24,10 @@ router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
 
-
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
 
