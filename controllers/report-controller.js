3ddc750f8a014dc14d1cfdd0586b0ac5ecdec909
Glitch (hammerhead-mercurial-collard) <none>
2024-08-27 15:55:07 +0000

    🎯⛲️ Checkpoint ./controllers/report-controller.js:69129952/5620

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -2,7 +2,7 @@ import { stationStore } from "../models/station-store.js";
 import { reportStore } from "../models/report-store.js";
 
 export const reportController = {
-  async index(request, response){
+  async index(request, response) {
     const stationId = request.params.stationid;
     const reportId = request.params.reportid;
     console.log(`Editing Report ${reportId} from Station ${stationId}`);
@@ -13,7 +13,7 @@ export const reportController = {
     };
     response.render("report-view", viewData);
   },
-  
+
   async addReport(request, response) {
     const stationId = request.params.id;
     const newReport = {
@@ -32,11 +32,28 @@ export const reportController = {
     const reports = await reportStore.getReportsByStationId(stationId);
     response.render("station-view", { stationId, reports });
   },
+
   async deleteReport(request, response) {
     const stationId = request.params.stationid;
     const reportId = request.params.reportid;
     console.log(`Deleting Report ${reportId} from Station ${stationId}`);
-    await reportStore.deleteReport(request.params.reportid);
+    await reportStore.deleteReport(reportId);
+    response.redirect("/station/" + stationId);
+  },
+
+  async update(request, response) {
+    const stationId = request.params.stationid;
+    const reportId = request.params.reportid;
+    const updatedReport = {
+      code: request.body.code,
+      temperature: request.body.temperature,
+      windSpeed: request.body.windSpeed,
+      windDirection: request.body.windDirection,
+      pressure: request.body.pressure,
+    };
+    console.log(`Updating Report ${reportId} from Station ${stationId}`);
+    const report = await reportStore.getReportById(reportId);
+    await reportStore.updateReport(report, updatedReport);
     response.redirect("/station/" + stationId);
   },
 };
