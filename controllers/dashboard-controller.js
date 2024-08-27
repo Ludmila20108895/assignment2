18621275490e267fc9d0efd92bbb369c1f6e1cf1
Glitch (hammerhead-mercurial-collard) <none>
2024-08-27 19:18:07 +0000

    🍄🍿 Checkpoint ./controllers/dashboard-controller.js:69129952/771

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,6 +1,8 @@
 import { stationStore } from "../models/station-store.js";
+import { accountsController } from "./accounts-controller.js";
 
 export const dashboardController = {
+  
   async index(request, response) {
     const viewData = {
       title: "Weather Station Dashboard",
@@ -25,5 +27,25 @@ export const dashboardController = {
     await stationStore.deleteStationById(stationId);
     response.redirect("/dashboard");
 },
+  async index(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
+    const viewData = {
+        title: "Station Dashboard",
+        stations: await stationStore.getStationsByUserId(loggedInUser._id),
+    };
+    console.log("dashboard rendering");
+    response.render("dashboard-view", viewData);
+},
+async addStation(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
+    const newStation = {
+        title: request.body.title,
+        userid: loggedInUser._id,
+    };
+    console.log(`adding station ${newStation.title}`);
+    await stationStore.addStation(newStation);
+    response.redirect("/dashboard");
+},
+
 
 };
