f8bceee525be1be427c15d53e0193a4bf4871833
Glitch (hammerhead-mercurial-collard) <none>
2024-08-27 14:58:32 +0000

    🐮🚋 Checkpoint ./routes.js:69129952/645

--- routes.js
+++ routes.js
@@ -2,10 +2,8 @@ import express from "express";
 import { accountsController } from './controllers/accounts-controller.js';
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { stationController } from "./controllers/station-controller.js";
-import { aboutController } from "./controllers/about-controller.js";
-
 import { reportController } from "./controllers/report-controller.js";
-
+import { aboutController } from "./controllers/about-controller.js";
 
 export const router = express.Router();
 
@@ -20,19 +18,21 @@ router.post("/authenticate", accountsController.authenticate);
 
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
-router.get("/about", aboutController.index);
+router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 router.get("/station/:id", stationController.index);
 
+router.post("/station/:id/addreport", stationController.addReport);
+router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
 
 router.post("/station/:id/delete", stationController.deleteStation);
 router.post("/station/:id/update", stationController.updateStation);
 
-router.post("/station/:id/addreport", reportController.addReport);
+
 router.get("/station/:id/reports", reportController.viewReports);
 
-router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 
-router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
+router.get("/about", aboutController.index);
+
 
 
 
