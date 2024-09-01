53425318622d28a8f65d601ac1eebe6998befbe5
Glitch (release3-weather2) <none>
2024-09-01 13:39:53 +0000

    🛩🔨 Checkpoint ./server.js:69129952/1268

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-export const stationController = {
-    async index(request, response) {
-        const station = await stationStore.getStationById(request.params.id);
-        const reports = await reportStore.getReportsByStationId(station._id);
-        const viewData = {
-            title: "Station",
-            station: station,
-            reports: reports, // Ensure reports are included in the view data
-        };
-        response.render("station-view", viewData); // Render station-view with reports
-    },
-    // Other methods...
-};
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { engine } from "express-handlebars";
+import { router } from "./routes.js";
+
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(fileUpload());
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use("/", router);
+
+const listener = app.listen(process.env.PORT || 4000, function () {
+  console.log(`Todolist started on http://localhost:${listener.address().port}`);
+});
+console.log(`Views path is set to: ${app.get("views")}`);
